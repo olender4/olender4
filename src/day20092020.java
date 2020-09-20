765ba53683db3fbabf93bfcfdbe183e4b4376498
olender4 <kontrolaitd@wp.pl>
2020-09-20 09:39:21 +0200

    'solution do ex1'

---
+++ src/day20092020.java
@@ -0,0 +1,12 @@
+public class day20092020 {
+    public static void main(String[] args) {
+        // program do wyswietlania tabliczki mnozenia
+
+        int x,y;
+        for (x =1; x<10;  x++){
+            for (y=1; y<10; y++){
+                System.out.println(x*y + " ");
+        }System.out.println();   }
+
+    }
+}
